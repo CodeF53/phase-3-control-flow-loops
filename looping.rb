2f9386da86c54f63d9a4538eea2dfb62401ec189
ChaseWelsh <fseusb@gmail.com>
2022-09-19 10:57:42 -0600

    puts is not intuitive

--- looping.rb
+++ looping.rb
@@ -1,24 +1,40 @@
 def happy_new_year
-  # your code here
+  count = 10
+  until count == 0
+    puts count
+    count = count-1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
+# TOO BAD, ITS UGLY
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
+  out = ""
+  if num % 3 == 0
+    out += "Fizz"
   end
+  if num % 5 == 0
+    out += "Buzz"
+  end
+  if out == ""
+    return num
+  end
+  return out
 end
 
 def fizzbuzz_printer
-  # your code here
+  100.times do |num|
+    puts fizzbuzz(num+1)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  out = ""
+  str.length.times do |i|
+    out = str[i] + out
+  end
+  return out
 end
+
+puts reverse_string("hello world")
